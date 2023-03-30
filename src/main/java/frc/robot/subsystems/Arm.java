57520ef595d0863e66f7f903e1afebeedd82a646
JerseyVoltage4587 <info@jerseyvoltage.com>
2023-03-29 22:03:52 -0500

    Tryna make PID work

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -4,6 +4,7 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
@@ -11,21 +12,30 @@ import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import frc.robot.OI;
 
 public class Arm extends SubsystemBase {
   public boolean isActive = true;
   static Arm Instance = null;
-  private WPI_TalonSRX armMotor;
+  public WPI_TalonSRX armMotor;
   private int mode = Constants.IntakeOFF_MODE;
   private boolean deployed = false;
   Encoder m_Encoder;
+  private OI m_controller;
   /** Creates a new Arm. */
   
   public Arm() {
     if (isActive == false) {
       return;
     }
+    m_controller = OI.getInstance();
     armMotor = new WPI_TalonSRX(8);
+    armMotor.configFactoryDefault();
+    armMotor.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative);
+    armMotor.setSensorPhase(true);
+    // armMotor.configMotionCruiseVelocity(Constants.kCruiseVelocity, 30);
+    // armMotor.configMotionAcceleration(Constants.kCruiseAcceleration, 30);
+    // armMotor.set(ControlMode.MotionMagic, 25000);
     //armMotor.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 10);
     //armMotor.setSelectedSensorPosition(0);
 
@@ -52,22 +62,36 @@ public class Arm extends SubsystemBase {
   }
 
   public void midCone() {
+    // if (m_controller.k.getRawButton(2)) {
+    //   double targetPos = OI.getInstance().k.getRawAxis(1) * 4096 * 10.0;
+    //   armMotor.set(ControlMode.MotionMagic, targetPos);
 
+    //   System.out.println("*****");
+    //   System.out.println(targetPos);
+    //   System.out.println(armMotor.getClosedLoopError(0));
+
+    // } else {
+    //   armMotor.set(ControlMode.PercentOutput, OI.getInstance().k.getRawAxis(1));
+    // }
   }
 
   public void midCube() {
-
+    // armMotor.set(ControlMode.MotionMagic, );
   }
 
   public void highCone() {
-
+    //armMotor.set(ControlMode.MotionMagic, );
   }
 
   public void highCube() {
-
+    //armMotor.set(ControlMode.MotionMagic, );
   }
 
-  public void setZero() {
+  public void setArmPositionZero() {
+    //armMotor.set(ControlMode.MotionMagic, 0);
+  }
+
+  public void setArmSpeedZero() {
     armMotor.set(0);
   }
 
